cfc24459958d43c85646fcf548a07a7616fa3f82
Oleksiy Slyshyk <alexslyshyk@gmail.com>
2024-12-03 19:15:56 +0200

    Implement dynamic day display and performance metrics for Day 4 to Day 7 programs

--- Day-04/Program.fs
+++ Day-04/Program.fs
@@ -1,2 +1,29 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.Diagnostics
+open Utils
+
+let day = 
+    Environment.GetCommandLineArgs() 
+    |> Array.take 1 
+    |> (fun x -> System.IO.Path.GetFileNameWithoutExtension(x.[0]))
+    |> (fun x -> x.Replace("-"," "))
+
+Say.printGreen($"\n\n\nAdvent of code 2024 {day}\n\n")
+
+// *** Tests ***
+
+
+// *** Problem ***
+Say.setVerbosity 0
+printf "\n---\n\n"
+let allocated = GC.GetAllocatedBytesForCurrentThread()
+let stopWatch = Stopwatch.StartNew()
+
+
+
+
+stopWatch.Stop()
+Say.printForce $"\n\n{day}: \n"
+Say.printForce $"    Execution time: {stopWatch.Elapsed.TotalMilliseconds} ms\n"
+Say.printForce $"    Memory used: {(GC.GetAllocatedBytesForCurrentThread() - allocated)} B\n"
+
